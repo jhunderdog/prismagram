7d3c97a78a88a771a6ad0b0e1342d9faff5ec786
jhunderdog <33382water@gmail.com>
2020-08-04 19:58:58 +0900

    toggleLike Resolver part One

---
+++ src/api/Post/ToggleLike/toggleLike.js
@@ -0,0 +1,48 @@
+import { mapAsyncIterator } from "graphql-tools";
+import { isAuthenticated } from "../../../middleware";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    toggleLike: async (_, args, { request }) => {
+      isAuthenticated(request);
+      const { postId } = args;
+      const { user } = request;
+      try {
+        const existingLike = await prisma.$exists.like({
+          AND: [
+            {
+              user: {
+                id: user.id,
+              },
+            },
+            {
+              post: {
+                id: postId,
+              },
+            },
+          ],
+        });
+        if (existingLike) {
+          // TO DO
+        } else {
+          await prisma.createLike({
+            user: {
+              connect: {
+                id: user.id,
+              },
+            },
+            post: {
+              connect: {
+                id: postId,
+              },
+            },
+          });
+        }
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+};
