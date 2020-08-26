bb93c88576af7ded1a397eac2d421484f23d9a55
jhunderdog <33382water@gmail.com>
2020-08-26 19:48:27 +0900

    createAccount Mutation part Two

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -4,15 +4,15 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     requestSecret: async (_, args, { request }) => {
-      console.log(request.user);
       const { email } = args;
       const loginSecret = generateSecret();
+
       try {
-        throw Error();
         await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
-      } catch {
+      } catch (e) {
+        console.log(e);
         return false;
       }
     },
